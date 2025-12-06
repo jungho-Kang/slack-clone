0f58222f7af7618705acd4a08b48cf691dd587f1
jungho-Kang <rkdwjdgh08@gmail.com>
2025-12-07 02:18:05 +0900

    [feat] 멤버 프로필 상세정보 확인기능 추가

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -72,7 +72,7 @@ export const Message = ({
   threadName,
   threadTimestamp,
 }: MessageProps) => {
-  const { parentMessageId, onOpenMessage, onClose } = usePanel();
+  const { parentMessageId, onOpenMessage, onOpenProfile, onClose } = usePanel();
 
   const [ConfirmDialog, confirm] = useConfirm(
     "Delete message",
@@ -86,7 +86,7 @@ export const Message = ({
   const { mutate: toggleReaction, isPending: isTogglingReaction } =
     useToggleReaction();
 
-  const isPending = isUpdatingMessage;
+  const isPending = isUpdatingMessage || isTogglingReaction;
 
   const handleReaction = (value: string) => {
     toggleReaction(
@@ -214,7 +214,10 @@ export const Message = ({
         )}
       >
         <div className="flex items-start gap-2">
-          <button>
+          <button
+            onClick={() => onOpenProfile(memberId)}
+            className="cursor-pointer"
+          >
             <Avatar>
               <AvatarImage src={authorImage} />
               <AvatarFallback>{avatarFallback}</AvatarFallback>
@@ -234,8 +237,8 @@ export const Message = ({
             <div className="flex flex-col w-full overflow-hidden">
               <div className="text-sm">
                 <button
-                  onClick={() => {}}
-                  className="font-bold text-primary hover:underline"
+                  onClick={() => onOpenProfile(memberId)}
+                  className="font-bold text-primary hover:underline cursor-pointer"
                 >
                   {authorName}
                 </button>
